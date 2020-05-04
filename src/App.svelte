2b8f6275ac714f5d3632d10050bcff1b451d55a1
Adrian Gaudebert <adrian@gaudebert.fr>
2020-05-04 09:59:48 +0200

    Show simple hexagonal tiles with content.

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,22 @@
 <script>
-	export let name;
+	import HexTile from './HexTile.svelte';
 </script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 </style>
+
+<main>
+	<div>
+		<HexTile content="F1"></HexTile>
+		<HexTile content="W2"></HexTile>
+		<HexTile content="S3"></HexTile>
+	</div>
+	<div>
+		<HexTile></HexTile>
+		<HexTile></HexTile>
+		<HexTile></HexTile>
+	</div>
+</main>
---
+++ src/HexTile.svelte
@@ -0,0 +1,62 @@
+<script>
+    export let content = '';
+</script>
+
+<style>
+    /* Source: https://codepen.io/gpyne/pen/iElhp */
+    .hexagon {
+        font-size: 0.1vmin;
+        position: relative;
+        display: inline-block;
+        margin: 1em 18em;
+        background-color: hsl(220, 75%, 75%);
+        text-align: center;
+    }
+    .hexagon,
+    .hexagon::before,
+    .hexagon::after {
+        width: 67em;
+        height: 116em;
+        border-radius: 20%/5%;
+    }
+    .hexagon::before {
+        background-color: inherit;
+        content: "";
+        position: absolute;
+        left: 0;
+        transform: rotate(-60deg);
+    }
+    .hexagon::after {
+        background-color: inherit;
+        content: "";
+        position: absolute;
+        left: 0;
+        transform: rotate(60deg);
+    }
+    .hexagon:nth-child(even) {
+        top: 59em;
+    }
+    .hexagon:hover {
+        background-color: hsla(60, 75%, 75%, 1.0);
+        cursor: pointer;
+        z-index: 105;
+    }
+    .hexagon:active {
+        background-color: hsla(60, 75%, 50%, 1.0);
+        z-index: 110;
+    }
+    .hexagon > span {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 140%;
+        font-size: 4vmin;
+        line-height: 1.2;
+        z-index: 100;
+    }
+</style>
+
+<div class="hexagon">
+    <span>{content}</span>
+</div>
