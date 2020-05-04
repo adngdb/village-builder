599097de8adbcf5fe3328f819ab1103093207977
Adrian Gaudebert <adrian@gaudebert.fr>
2020-05-04 17:34:18 +0200

    Add new building types: sawmill and quarry.

--- src/data/buildings.js
+++ src/data/buildings.js
@@ -1,7 +1,7 @@
 const TYPES = {
     FARM: 'buildings/FARM',
-    // SAWMILL: 'buildings/SAWMILL',
-    // QUARRY: 'buildings/QUARRY',
+    SAWMILL: 'buildings/SAWMILL',
+    QUARRY: 'buildings/QUARRY',
 };
 
 
@@ -16,6 +16,44 @@ export default {
             3: { food: 20 },
             4: { food: 50 },
             5: { food: 100 },
+        },
+        cost: {
+            0: { wood: 10 },
+            1: { wood: 30 },
+            2: { wood: 80 },
+            3: { wood: 200 },
+            4: { wood: 500 },
+            5: { wood: 1000 },
+        },
+    },
+    [TYPES.SAWMILL]: {
+        name: 'Sawmill',
+        maxLevel: 5,
+        output: {
+            1: { wood: 5 },
+            2: { wood: 10 },
+            3: { wood: 20 },
+            4: { wood: 50 },
+            5: { wood: 100 },
+        },
+        cost: {
+            0: { wood: 10 },
+            1: { wood: 30 },
+            2: { wood: 80 },
+            3: { wood: 200 },
+            4: { wood: 500 },
+            5: { wood: 1000 },
+        },
+    },
+    [TYPES.QUARRY]: {
+        name: 'Quarry',
+        maxLevel: 5,
+        output: {
+            1: { stone: 5 },
+            2: { stone: 10 },
+            3: { stone: 20 },
+            4: { stone: 50 },
+            5: { stone: 100 },
         },
         cost: {
             0: { wood: 10 },
