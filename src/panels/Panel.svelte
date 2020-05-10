243f2ff61926ac2bd72044dc10ef577f77405f48
Adrian Gaudebert <adrian@gaudebert.fr>
2020-05-10 19:58:56 +0200

    Close panel when clicking outside of it.

--- src/panels/Panel.svelte
+++ src/panels/Panel.svelte
@@ -6,6 +6,25 @@
     function cancel() {
         dispatch('cancel');
     }
+
+    // From https://svelte.dev/repl/0ace7a508bd843b798ae599940a91783?version=3.16.7
+    function clickOutside(node) {
+        const handleClick = event => {
+            if (node && !node.contains(event.target) && !event.defaultPrevented) {
+                node.dispatchEvent(
+                    new CustomEvent('click_outside', node)
+                )
+            }
+        }
+
+        document.addEventListener('click', handleClick, true);
+
+        return {
+            destroy() {
+                document.removeEventListener('click', handleClick, true);
+            }
+        }
+    }
 </script>
 
 <style>
@@ -27,8 +46,8 @@
     }
 </style>
 
-<section on:click={ cancel }>
-    <div class="panel" on:click|stopPropagation={ () => {} }>
+<section>
+    <div class="panel" use:clickOutside on:click_outside={ cancel }>
         <h2>
             <span on:click={ cancel } class="close">
                 <img class="icon" src="img/ui/cancel.svg" alt="" />
